32326849c7d86be9cbfcbab3ec251229c0c4018a
Malik Kennedy <mksybr@gmail.com>
2024-09-12 15:45:08 -0400

    feat: add, remove, inorder, preorder and postorder

--- binary-search-tree/src/main/java/com/mksybr/sdc/cs201/BinarySearchTree.java
+++ binary-search-tree/src/main/java/com/mksybr/sdc/cs201/BinarySearchTree.java
@@ -2,10 +2,13 @@ package com.mksybr.sdc.cs201;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class BinarySearchTree {
+    static private Tree<Integer> tree = new Tree<Integer>();
+    static private Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         // Tree t = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)));
         // System.out.println(t.toStringInOrder());
@@ -22,51 +25,83 @@ public class BinarySearchTree {
         // System.out.println(f.toStringPostOrder());
 
         while(true) {
-            menu();
+            System.out.println("1) Create a binary search tree\r\n" + //
+            "2) Add a node\r\n" + //
+            "3) Delete a node\r\n" + //
+            "4) Print nodes by InOrder\r\n" + //
+            "5) Print nodes by PreOrder\r\n" + //
+            "6) Print nodes by PostOrder\r\n" + //
+            "7) Exit program");
+            var choice = scanner.nextInt();
+            menu(choice);
         }
     }
 
-    static void menu() {
-        var scanner = new Scanner(System.in);
-        System.out.println("1) Create a binary search tree\r\n" + //
-                        "2) Add a node\r\n" + //
-                        "3) Delete a node\r\n" + //
-                        "4) Print nodes by InOrder\r\n" + //
-                        "5) Print nodes by PreOrder\r\n" + //
-                        "6) Print nodes by PostOrder\r\n" + //
-                        "7) Exit program");
-        var choice = scanner.nextInt();
+    static void menu(int choice) {
         switch(choice) {
+            case 1, 2 -> {
+                readIntegers().forEach(i -> {
+                    tree.add(i);
+                });
+            } 
+            case 3 -> {
+                readIntegers().forEach(i -> {
+                    tree.remove(i);
+                });
+            } 
+            case 4 -> {
+                System.out.println(tree.inorder());
+            } 
+            case 5 -> {
+                System.out.println(tree.preorder());
+            } 
+            case 6 -> {
+                System.out.println(tree.postorder());
+            } 
             case 7 -> {
                 scanner.close();
                 System.exit(0);
             }
-            default -> System.out.println("Invalid option selected.");
         }
     }
-    
+    static List<Integer> readIntegers() {
+        List<Integer> result = Collections.emptyList();
+        if(scanner.hasNextLine()) {
+            try {
+                result = Arrays.asList(scanner.nextLine().split(" +"))
+                .stream()
+                .map(i -> Integer.parseInt(i))
+                .toList();        
+            } catch(java.lang.NumberFormatException e) {
+                return readIntegers();
+            }
+        }
+        return result;
+    }
 }
 
-class Tree<T extends Comparable<T>> {
-    private Node<T> root; 
-    private int depth;
+class Tree<T extends Comparable<T>> implements Comparable<Tree<T>> {
+    public Node<T> root; 
+    Tree() {}
+    Tree(Node<T> root) {
+        this.root = root;
+    }
     Tree(Iterable<T> initial) {
         for(var i : initial) {
             add(i);
         }
     }
     
-    public Tree<T> add(T t) {
+    public void add(T t) {
         if(null == root) {
             root = new Node<T>(t);
         } else {
             root.add(t);
         }
-        return null;
     }
 
     public void remove(T t) {
-        root.remove(t);
+        this.root.remove(t, null);
     }
 
     @Override
@@ -75,32 +110,52 @@ class Tree<T extends Comparable<T>> {
         return sb.toString();
     }   
 
-    public List<Node<T>> inorder() {
+    public List<T> inorder() {
         return root.inorder();
     }
 
     
-    public List<Node<T>> preorder() {
+    public List<T> preorder() {
         return root.preorder();
     }
 
     
-    public List<Node<T>> postorder() {
+    public List<T> postorder() {
         return root.postorder();
     }
+    @Override
+    public int compareTo(Tree<T> other) {
+        
+        var accumulator = 0;
+        return this.compareTo(other, accumulator);
+    }
 
+    private int compareTo(Tree<T> other, int accumulator) {
+        accumulator += this.root.compareTo(other.root);
+        if(this.root.getLeft() != null && other.root.getLeft() != null)
+            accumulator += this.root.getLeft().compareTo(other.root.getLeft());
+        else if (this.root.getLeft() != null)
+            accumulator += -1;
+        else if(other.root.getLeft() != null)
+            accumulator += 1;
+        
+        if(this.root.getRight() != null && other.root.getRight() != null)
+            accumulator += this.root.getRight().compareTo(other.root.getRight());
+        else if(this.root.getRight() != null)
+            accumulator += -1;
+        else if(other.root.getRight() != null)
+            accumulator += 1;
+            
+        return Long.signum(accumulator);
+    }
 }
+
 class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
     private T t;
-    private Node parent;
-    private Node<T> left;
-    private Node<T> right;
+    public Node<T> left;
+    public Node<T> right;
     Node(T t) {
         this.t = t;
-    }
-    Node(Node<T> parent, T t) {
-        this.t = t;
-        this.parent = parent;
     }
     public T getValue() {
         return t;
@@ -109,12 +164,15 @@ class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
         return (null == left ? 0 : 1) + (null == right ? 0 : 1);
     }
     public void add(T addition) {
+        if(this.t == null)
+            this.t = addition;
+
         if(0 < t.compareTo(addition)) {
             if(left == null)
                 addLeft(addition);
             else
                 left.add(addition);
-        } else if (0 > t.compareTo(addition)) {
+        } else if (0 >= t.compareTo(addition)) {
             if(right == null)
                 addRight(addition);
             else
@@ -122,77 +180,53 @@ class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
         }
     }
 
-    public void remove(T t) {
-        var removal = new Node<T>(t);
-        // search matches on root
-        if(0 == this.compareTo(removal) && this.getChildrenCount() == 0) {
-            this.t = null;
-            return;
-        // search matches on element with 1 child
-        } else if(0 == this.compareTo(removal) && this.getChildrenCount() == 1) {
+    public void remove(T t, Node<T> parent) {
+        Node<T> removal = new Node<T>(t);
+        if(0 < this.compareTo(removal)) {
             if(this.getLeft() != null) {
-                this.t = (T) this.getLeft().getValue();
-            } else if(this.getRight() != null) {
-                this.t = (T) this.getRight().getValue();
-            } else {
-                throw new RuntimeException("Node reports 1 child but both left and right are null.");
-            }
-        // search matches on element with 2 children
-        } else if(0 == this.compareTo(removal) && this.getChildrenCount() == 2) {
-            this.t = this.successor(removal).getValue();
-        // no match found
+                this.left.remove(t, this);
+            } else
+                return;
+        } else if(0 > this.compareTo(removal)) {
+            if(this.getRight() != null) {
+                this.right.remove(t, this);
+            } else 
+                return;
         } else {
-            if(0 > this.compareTo(removal)) {
-                this.getLeft().remove(t);
-            } else if(0 < this.compareTo(removal)) {
-                this.getRight().remove(t);
+            if (this.getChildrenCount() == 2) {
+                this.t = parent.successor();
+            } else if(parent.left.t == this.t) {
+                parent.left = (this.left != null) ? left : right;
+            } else if(parent.right.t == this.t) {
+                parent.right = (this.left != null) ? left : right;
             } 
+            return;
         }
     }
-
-    // public void remove(T removal) {
-    //     var node = new Node(removal);
-    //     if(0 == this.compareTo(node)) {
-    //         this.left = null;
-    //         this.right = null;
-    //     } else if(0 < this.compareTo(node)) {
-    //         if(0 == this.getLeft().compareTo(node)) {
-    //             this.left = null;
-    //         } else {
-    //             this.getLeft().remove(removal);
-    //         }
-    //     } else if(0 > this.compareTo(node)) {
-    //         if(0 == this.getRight().compareTo(node)) {
-    //             this.right = null;
-    //         } else {
-    //             this.getRight().remove(removal);
-    //         }
-    //     }
-    // }
-
-    public List<Node<T>> inorder() {
+    
+    public List<T> inorder() {
         return this.inorder(null);
     }
 
-    public List<Node<T>> inorder(List<Node<T>> accumulator) {
+    public List<T> inorder(List<T> accumulator) {
         if(null == accumulator)
-            accumulator = new ArrayList<Node<T>>();
+            accumulator = new ArrayList<T>();
         if(this.getLeft() != null)
             this.getLeft().inorder(accumulator);
-        accumulator.add(this);
+        accumulator.add(this.t);
         if(this.getRight() != null)
             this.getRight().inorder(accumulator);
         return accumulator;
     }
 
-    public List<Node<T>> preorder() {
+    public List<T> preorder() {
         return this.preorder(null);
     }
 
-    public List<Node<T>> preorder(List<Node<T>> accumulator) {
+    public List<T> preorder(List<T> accumulator) {
         if(null == accumulator)
-            accumulator = new ArrayList<Node<T>>();
-        accumulator.add(this);
+            accumulator = new ArrayList<T>();
+        accumulator.add(this.getValue());
             if(this.getLeft() != null)
             this.getLeft().preorder(accumulator);
         if(this.getRight() != null)
@@ -200,45 +234,51 @@ class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
         return accumulator;
     }
 
-    public List<Node<T>> postorder() {
+    public List<T> postorder() {
         return this.postorder(null);
     }
 
-    public List<Node<T>> postorder(List<Node<T>> accumulator) {
+    public List<T> postorder(List<T> accumulator) {
         if(null == accumulator)
-            accumulator = new ArrayList<Node<T>>();
+            accumulator = new ArrayList<T>();
         if(this.getLeft() != null)
             this.getLeft().postorder(accumulator);
         if(this.getRight() != null)
             this.getRight().postorder(accumulator);
-        accumulator.add(this);
+        accumulator.add(this.getValue());
         return accumulator;
     }
-    public Node<T> predecessor() {
+    public T predecessor() {
         return this.predecessor(null);
     }
-    public Node<T> predecessor(Node<T> key) {
+    public T predecessor(Node<T> key) {
         if(key == null) {
             key = this;
         }
-        List<Node<T>> inorder = inorder(null);
+        List<T> inorder = inorder(null);
         if(inorder.get(inorder.indexOf(key) - 1) != null)
             return inorder.get(inorder.indexOf(key) - 1);
         else return null;
     }
 
-    public Node<T> successor() {
+    public T successor() {
         return this.successor(null);
     }
 
-    public Node<T> successor(Node<T> key) {
+    public T successor(Node<T> key) {
         if(key == null) {
             key = this;
         }
-        List<Node<T>> inorder = inorder(null);
-        if(inorder.get(inorder.indexOf(key) - 1) != null)
-            return inorder.get(inorder.indexOf(key) + 1);
-        else return null;    }
+        List<T> inorder = inorder(null);
+        try {
+            if(inorder.get(inorder.indexOf(key) - 1) != null)
+                return inorder.get(inorder.indexOf(key) + 1);
+            else return null;    
+        } catch (IndexOutOfBoundsException e) {
+            return inorder.get(0);
+        }
+    
+    }
 
     public Node<T> getLeft() {
         return left;
@@ -248,7 +288,7 @@ class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
     }    
     public void addLeft(T left) {
         if(null == this.left) {
-            this.left = new Node(left);
+            this.left = new Node<T>(left);
         } else {
             // throw new IndexOutOfBoundsException("Node already has left child.");
         }
@@ -256,11 +296,12 @@ class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
     }
     public void addRight(T right) {
         if(null == this.right) {
-            this.right = new Node(right);
+            this.right = new Node<T>(right);
         } else {
             // throw new IndexOutOfBoundsException("Node already has right child.");
         }
     }
+
     @Override
     public int compareTo(Node<T> other) {
         return t.compareTo(other.getValue());
--- binary-search-tree/src/test/java/com/mksybr/sdc/cs201/BinarySearchTreeTest.java
+++ binary-search-tree/src/test/java/com/mksybr/sdc/cs201/BinarySearchTreeTest.java
@@ -1,6 +1,5 @@
 package com.mksybr.sdc.cs201;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,12 +8,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BinaryTreeSearchTests {
     @Test
-    public void removal() {
-        // https://www.algolist.net/Data_structures/Binary_search_tree/Removal
+    public void removeLeafNode() {
         var subject = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,12,9,21,19,25))); 
-        var expected = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,19,9,21,25))); 
+        var expected = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,12,9,21,19))); 
+
+        subject.remove(25);
+        assertEquals(0, expected.compareTo(subject));
+    }
+
+    @Test
+    public void removeNodeWithOneChild() {
+        var subject = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,12,9,21,19,25))); 
+        var expected = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,12,9,19,25))); 
         
-        subject.remove(19);
-        assertEquals(subject, expected);
+        subject.remove(21);
+        assertEquals(0, expected.compareTo(subject));
+    }
+
+
+    @Test
+    public void removeNodeWithTwoChildren() {
+        var subject = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,12,9,21,19,25))); 
+        var expected = new Tree<Integer>(new ArrayList<Integer>(Arrays.asList(5,2,-4,3,12,9,19,25))); 
+        
+        subject.remove(21);
+        assertEquals(0, expected.compareTo(subject));
+    }
+
+    @Test
+    public void creation() {
+        // https://www.algolist.net/Data_structures/Binary_search_tree/Removal
+        var subject = new Tree<Integer>(new Node<Integer>(1));
+        var expected = new Tree<Integer>(Arrays.asList(1));
+        
+        assertEquals(0, expected.compareTo(subject));
+        assertEquals(0, subject.compareTo(expected));
+
+        subject = new Tree<Integer>(new Node<Integer>(1));
+        subject.add(2);
+        expected = new Tree<Integer>(Arrays.asList(1,2));
+        assertEquals(0, expected.compareTo(subject));
     }
 }
